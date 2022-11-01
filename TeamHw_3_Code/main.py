36a8593a0f046f2b7930fd540564d2b83c949195
Yunseo Lee <lys3269@dgu.ac.kr>
2022-11-01 09:37:04 +0900

    ADD languages function

--- TeamHw_3_Code/main.py
+++ TeamHw_3_Code/main.py
@@ -15,7 +15,7 @@ def result():
       # 성별
       result['Gender'] = request.form.get('Gender')
       # 학과
-      # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
+      checklist = request.form.getlist('mycheckbox')
 
       return render_template("result.html",result = result)
 
